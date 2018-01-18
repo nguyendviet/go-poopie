348914e92f643f0169536b52849ece56df44e52e
dailybp <bpdailey@gmail.com>
2018-01-17 20:23:48 -0500

    Fixed Map.js

--- client/src/pages/Map/Map.js
+++ client/src/pages/Map/Map.js
@@ -1,38 +1,37 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {
+  withGoogleMap,
+  GoogleMap,
+  InfoWindow,
+  Marker,
+} from "google-maps-react";
 
-export class Map extends React.Component {
-
-//list of event listener names
 const evtNames = ['click', 'dragend'];
-//Functions=====================================================================
-//Function that checks to see if the map component should update based on
-//current location.
-  componentDidUpdate(prevProps, prevState) {
-        if (prevProps.google !== this.props.google) {
-          this.loadMap();
-        }
-        if (prevState.currentLocation !== this.props.google){
-          this.recenterMap();
-        }
-      }
-//
-  loadMap() {
-    if (this.props && this.props.google) {
-      const {google} = this.props;
+
+export class Map extends React.Component{
+
+//Functions
+//==============================================================================
+  loadMap(){
+    if(this.props && this.props.google){
+      //google is available
+      const{google}=this.props;
       const maps = google.maps;
 
       const mapRef = this.refs.map;
       const node = ReactDOM.findDOMNode(mapRef);
 
+      // let zoom = 14;
+      // let lat = 38.881487;
+      // let lng = -77.116197;
       let {initialCenter, zoom} = this.props;
-          //const {lat, lng} = initialCenter;
+      // const {lat, lng} = initialCenter;
       const {lat, lng} = this.state.currentLocation;
-      const center = new maps.LatLng(lat, lng);
+      const center = new maps.LatLng(lat,lng);
       const mapConfig = Object.assign({}, {
-          center: center,
-          zoom: zoom
+        center: center,
+        zoom: zoom
       })
-
+      //the maps.Map() constructor accepts a DOM node and a configuration object to create a map
       this.map = new maps.Map(node, mapConfig);
 
       evtNames.forEach(e => {
@@ -40,8 +39,9 @@ const evtNames = ['click', 'dragend'];
       });
 
       let centerChangedTimeout;
-      this.map.addListener('dragend', (evt) => {
-        if (centerChangedTimeout) {
+
+      this.map.addListener('dragend', (evt) =>{
+        if (centerChangedTimeout){
           clearTimeout(centerChangedTimeout);
           centerChangedTimeout = null;
         }
@@ -52,39 +52,54 @@ const evtNames = ['click', 'dragend'];
     }
   }
 
-  recenterMap() {
+  //After a React component has updated this following methods will run.
+  componentDidUpdate(prevProps, prevState){
+    if(prevProps.google !== this.props.google){
+      this.loadMap();
+    }
+    //if the current location differs from the initial center
+    //then the recenterMap funciton will run.
+    if (prevState.currentLocation !== this.state.currentLocation){
+      this.recenterMap();
+    }
+  }
+
+  recenterMap(){
     const map = this.map;
     const curr = this.state.currentLocation;
 
     const google = this.props.google;
     const maps = google.maps;
 
-    if (map) {
-        let center = new maps.LatLng(curr.lat, curr.lng)
-        map.panTo(center)
+    if (map){
+      let center = new maps.LatLng(curr.lat, curr.lng)
+      map.panTo(center)
     }
   }
 
   handleEvent(evtName) {
     let timeout;
     const handlerName = 'on${camelize(evtName)}';
-    return(e) => {
-      if(timeout){
+
+    return (e) => {
+      if (timeout) {
         clearTimeout(timeout);
         timeout = null;
       }
-      timeout = setTimeout(() =>{
-        if(this.props[handlerName]){
+      timeout = setTimeout(() => {
+        if (this.props[handlerName]) {
           this.props[handlerName](this.props, this.map, e);
         }
       }, 0);
     }
   }
 
-  componentDidMount() {
-    if(this.props.centerAroundCurrentLocation){
-      if(navigator && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((pos) => {
+  //When the Map is available when the component mounts whenever the map has
+  //already been loaded previously. The folwing method will run.
+  componentDidMount(){
+    if(this.props.CenterAroundCurrentLocation){
+      if(navigator && navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((pos) =>{
           const coords = pos.coords;
           this.setState({
             currentLocation: {
@@ -98,48 +113,83 @@ const evtNames = ['click', 'dragend'];
     this.loadMap();
   }
 
+  //Camelize helper Function
   const camelize = function(str) {
     return str.split(' ').map(function(word){
       return word.charAt(0).toUpperCase() + word.slice(1);
     }).join('');
   }
 
-//Props=========================================================================
+  renderChildren(){
+    const {children} = this.props;
+
+    if (!children) return;
+
+    return React.Children.map(children, c =>{
+      return React.cloneElement(c, {
+        map: this.map,
+        google: this.props.google,
+        mapCenter: this.state.currentLocation
+      });
+    })
+  }
+
+//the following constructor allows the map to retain state
   constructor(props){
     super(props);
 
     const {lat, lng} = this.props.initialCenter;
     this.state = {
-      currentLocation: {
-        lat: lat,
-        lng: lng
-      }
+      lat: lat,
+      lng: lng
     }
   }
 
+//Props
+//==============================================================================
   Map.propTypes = {
-        google: React.PropTypes.object,
-        zoom: React.PropTypes.number,
-        initialCenter: React.PropTypes.object,
-        centerAroundCurrentLocation: React.PropTypes.bool,
-        onMove: React.PropTypes.func,
-    }
+    google: React.PropTypes.object,
+    zoom: React.PropTypes.number,
+    initialCenter: React.PropTypes.object,
+    CenterAroundCurrentLocation: React.PropTypes.bool,
+    onMove: React.PropTypes.func
+  }
 
   Map.defaultProps = {
-        zoom: 14,
-        initialCenter: {
-        lat: 38.881487,
-        lng: -77.116197
-        },
-        centerAroundCurrentLocation: false,
-        onMove: function() {}
-    }
+    zoom: 14,
+    //GW Arlington Campus location as default
+    initialCenter: {
+      lat: 38.881487,
+      lng: -77.116197
+    },
+    CenterAroundCurrentLocation: false,
+    onMove: function() {}
+  }
+//==============================================================================
 
   render(){
     return(
       <div ref='map'>
         Loading map...
+        {this.renderChildren()}
       </div>
     )
   }
 }
+
+
+
+// import React, {Component} from 'react';
+//
+// class Map extends Component {
+//     state = {
+//     };
+//
+//     render() {
+//         return (
+//             <p>This is where we show the map.</p>
+//         )
+//     }
+// }
+//
+// export default Map;
