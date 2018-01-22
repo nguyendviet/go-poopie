a58935b377b90d616a6f4b0fe8c2464e2780e412
Mike Yencha <jmyeniii@icloud.com>
2018-01-22 18:38:17 -0500

    still in progress

--- client/src/components/BigButton/BigButton.js
+++ client/src/components/BigButton/BigButton.js
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom';
 import './BigButton.css'
 
 const BigButton = () =>
-    <Link to="/map"><button type="button" className="btn btn-danger btn-lg btn-round">Big Button</button></Link>
+    <div className="bam">
+        <Link to="/map"><button type="button" className="btn btn-danger btn-lg btn-round">Big Button</button></Link>
+    </div>
 ;
 
 export default BigButton;
